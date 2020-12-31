ae0bc4c7bfdf25cc897842d6f8c4b2884bea6b1b
Petri Mattila <petri.mattila@unipart.io>
2020-12-31 17:57:59 +0000

    PMa: Refactor bed leveling

---
+++ Marlin/UltiLCD2_menu_leveling.h
@@ -0,0 +1,7 @@
+#ifndef ULTI_LCD2_MENU_LEVELING_H
+#define ULTI_LCD2_MENU_LEVELING_H
+
+void lcd_menu_start_bed_leveling();
+void lcd_menu_start_bed_adjustment();
+
+#endif//ULTI_LCD2_MENU_LEVELING_H
